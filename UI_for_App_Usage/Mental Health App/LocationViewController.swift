eece3a33eae945a5b91c3c232c6a17884a0238b4
Federico Brandt <fbrandt@purdue.edu>
2020-04-30 11:59:18 -0400

    Final push for the semester. Code updated with comments

--- UI_for_App_Usage/Mental Health App/LocationViewController.swift
+++ UI_for_App_Usage/Mental Health App/LocationViewController.swift
@@ -5,6 +5,11 @@
 //  Created by Federico Brandt on 3/12/20.
 //  Copyright © 2020 Federico Brandt. All rights reserved.
 //
+//GPS Data Extraction Screen
+//It is to be noted that this class' functionality is not complete
+//Data extraction works but it isn't written onto any file due to the fact that
+//it is yet to be implemented with a timer to extract data every set amount of minutes
+//Current model works to extract GPS coordinates (longitude and latitude) every second.
 
 import UIKit
 import CoreLocation
@@ -12,9 +17,12 @@ import MapKit
 
 class LocationViewController: UIViewController, CLLocationManagerDelegate {
     
+    //Map View Initialization
     @IBOutlet weak var Map_Loc: MKMapView!
+    //Location manager
     let manager = CLLocationManager()
-        
+    
+    //Function that extracts current coordinates for user and prints them to terminal
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         
@@ -28,17 +36,16 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
         self.Map_Loc.setRegion(region, animated: true)
         self.Map_Loc.showsUserLocation = true
         
-        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestAlwaysAuthorization()
-        manager.allowsBackgroundLocationUpdates = true
+        manager.desiredAccuracy = kCLLocationAccuracyBest //Set accuracy of location extraction
+        manager.requestAlwaysAuthorization() //Request for background app data extraction at all times
+        manager.allowsBackgroundLocationUpdates = true //Request for app running without being active
         manager.showsBackgroundLocationIndicator = false
-        manager.pausesLocationUpdatesAutomatically = false
-        manager.startUpdatingLocation()
+        manager.pausesLocationUpdatesAutomatically = false //Request to never stop location extraction
+        manager.startUpdatingLocation() //Initialize location extraction
         manager.delegate = self
 
         
